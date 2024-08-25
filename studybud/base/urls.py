687ebba8f6a22e1e47c2b3d5d2d560be94923598
Om Sakharkar <sakharkarom777@gmail.com>
2024-08-25 16:35:20 +0530

    base, template folder added

---
+++ studybud/base/urls.py
@@ -0,0 +1,9 @@
+# apps url file
+from django.urls import path
+from . import views
+# from django.http import HttpResponse
+
+urlpatterns  = [
+    path('', views.home, name="home"),
+    path('room/', views.room, name="room")
+]
